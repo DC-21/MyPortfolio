1ec5a9aefd5fba70ca5744308e8f54c6ad467a42
DC-21 <cholahkuboko@gmail.com>
2023-06-15 22:15:20 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,26 +7,26 @@ const Navbar = () => {
     setOpen(!open)
   }
   return (
-    <div className='flex w-full px-4 md:px-8 py-4 items-center justify-between' style={{background: '#cccfb2'}}>
+    <div className='flex w-full px-4 md:px-8 py-4 items-center justify-between transition-all bg-[#d6d8c4]'>
       <div className='flex gap-1 items-center'>
-        <img src={logo} alt='Logo' className='h-[28px] w-[64px]' style={{background: '#cccfb2'}}/>
+        <img src={logo} alt='Logo' className='h-[28px] w-[64px]' style={{background: '#d6d8c4'}}g/>
       </div>
       <ul className='md:flex hidden gap-6'>
-        <li>
+        <li className=' hover:border-b-[1px] border-black transition-all duration-300 px-2 py-1'>
           <a href=''>Home</a>
         </li>
-        <li>
+        <li className=' hover:border-b-[1px] border-black transition-all duration-300 px-2 py-1'>
           <a href=''>Projects</a>
         </li>
-        <li>
+        <li className=' hover:border-b-[1px] border-black transition-all duration-300 px-2 py-1'>
           <a href=''>About</a>
         </li>
       </ul>
-      <button class="button md:flex hidden">
+      <button class="button hover:fill-white md:flex hidden hover:text-white">
         <svg class="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
         Hire Me
       </button>
-      <div onClick={handleMenu}>
+      <div className='md:hidden block' onClick={handleMenu}>
         {open?<svg
           viewBox="0 0 470 1000"
           fill="currentColor"
@@ -43,7 +43,7 @@ const Navbar = () => {
           >
             <path d="M4 6h16v2H4zm0 5h16v2H4zm0 5h16v2H4z" />
           </svg>}
-          {open ? <ul className='absolute right-0 gap-6'>
+          {open ? <ul className='absolute right-0 px-2 gap-8 h-20 rounded bg-[#d2d3c9]'>
         <li>
           <a href=''>Home</a>
         </li>
