0b47b6b7e53c267dc4c576041fed0ce0a5e5327d
DC-21 <cholahkuboko@gmail.com>
2023-06-15 17:30:40 +0200

    first commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div>
+      <div>
+        <div>
+            <h1>Chola Kuboko</h1>
+            <h1>Web Developer for Hire</h1>
+        </div>
+        <div></div>
+      </div>
+      <div>
+        <div></div>
+        <div></div>
+      </div>
+    </div>
+  )
+}
+
+export default Hero
