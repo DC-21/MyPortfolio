8b1cb1ef8ca646d09bb22acfd1153df4f228cdbf
DC-21 <cholahkuboko@gmail.com>
2023-06-15 17:57:39 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import logo from '../images/Navbar/logo.jpg'
 
 const Navbar = () => {
   return (
     <div className='font-quicksand flex w-full px-8 py-4 justify-between'>
       <div className='flex gap-4'>
-        <a href=''>Logo</a>
+        <img src={logo}></img>
         <button class="button">
           <svg class="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
             Explore
