6cebe8b7b2a91f5c3fb96dee1dfd12f4598adbd9
DC-21 <cholahkuboko@gmail.com>
2023-06-16 17:04:16 +0200

    first commit

---
+++ src/components/Slide.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+import Slider from 'react-slick';
+import 'slick-carousel/slick/slick.css';
+import 'slick-carousel/slick/slick-theme.css';
+
+const Slide = () => {
+  // Define the websites to be embedded
+  const websites = [
+    'https://ai-chola.netlify.app/'
+  ];
+
+  // Configure the settings for the slider
+  const sliderSettings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  };
+
+  return (
+    <div className="flex justify-center w-full items-center h-screen">
+      <div className="w-full items-center h-screen justify-center">
+        <Slider {...sliderSettings}>
+          {websites.map((website, index) => (
+            <iframe
+              key={index}
+              title={`Website ${index + 1}`}
+              src={website}
+              className="w-full h-full"
+            />
+          ))}
+        </Slider>
+      </div>
+    </div>
+  );
+};
+
+export default Slide;
