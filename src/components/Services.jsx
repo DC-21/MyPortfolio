d03389733570a6d6b73e003085d7d3cdbed37721
DC-21 <cholahkuboko@gmail.com>
2023-06-16 15:29:20 +0200

    first commit

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -33,7 +33,7 @@ const Services = () => {
                                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                 <path d="M22 4L12 14.01l-3-3"></path>
                             </svg>
-                            <span class="title-font font-medium">Web Penetration Testing</span>
+                            <span class="title-font font-medium">Web Designing</span>
                         </div>
                     </div>
                     <div class="p-2 sm:w-1/2 w-full">
@@ -42,7 +42,7 @@ const Services = () => {
                                 <path d="M22 11.08V12a10 10 0 11-5.93-9.14"></path>
                                 <path d="M22 4L12 14.01l-3-3"></path>
                             </svg>
-                            <span class="title-font font-medium">hello</span>
+                            <span class="title-font font-medium">XSS Vulnerability</span>
                         </div>
                     </div>
                     <div class="p-2 sm:w-1/2 w-full">
