93bb157822d2d442d2925d9b3f622bc1ced1b6c9
DC-21 <cholahkuboko@gmail.com>
2023-06-16 12:07:05 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,26 @@
 import React from 'react'
+import { Player } from '@lottiefiles/react-lottie-player';
+
 
 const Hero = () => {
   return (
-    <div className='flex px-4 md:px-8  flex-wrap w-full h-screen'>
+    <div className='flex px-4 md:px-8  flex-wrap w-full h-screen pt-8'>
       <div className='md:w-1/2 w-full'>
         <div>
-            <h1>Chola Kuboko</h1>
-            <h1>Web Developer for Hire</h1>
+          <h1>Chola Kuboko</h1>
+          <h1>Web Developer for Hire</h1>
         </div>
         <div></div>
       </div>
       <div className='md:w-1/2 w-full'>
-        <div>hello</div>
+        <div>
+          <Player
+            autoplay
+            loop
+            src='https://assets3.lottiefiles.com/packages/lf20_Pr3rKjXraf.json'
+            style={{ width: '100%', height:'auto' }}
+          />
+        </div>
         <div></div>
       </div>
     </div>
