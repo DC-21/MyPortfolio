1850ca048290b86e97fa84176ac9513ba5660a6f
DC-21 <cholahkuboko@gmail.com>
2023-06-16 16:18:10 +0200

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Hero from './components/Hero'
 import About from './components/About'
 import Services from './components/Services'
 import Portfolio from './components/Portfolio'
+import Contact from './components/Contact'
 import Footer from './components/Footer'
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
      <About/>
      <Services/>
      <Portfolio/>
+     <Contact/>
      <Footer/>
     </>
   )
--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,35 +1,35 @@
 import React from 'react'
 
-const Footer = () => {
+const Contact = () => {
     return (
-        <section class="text-gray-600 body-font relative bg-slate-500">
-            <div class="container px-5 py-24 mx-auto">
+        <section class="w-full body-font relative bg-slate-500">
+            <div class="container px-5 py-10 mx-auto">
                 <div class="flex flex-col text-center w-full mb-12">
-                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-gray-900">Contact Us</h1>
-                    <p class="lg:w-2/3 mx-auto leading-relaxed text-base">Whatever cardigan tote bag tumblr hexagon brooklyn asymmetrical gentrify.</p>
+                    <h1 class="sm:text-3xl text-2xl font-medium title-font mb-4 text-white">Contact Us</h1>
+                    <p class="lg:w-2/3 mx-auto leading-relaxed text-base text-slate-300">Feel free to reach me in case you have a project in mind</p>
                 </div>
                 <div class="lg:w-1/2 md:w-2/3 mx-auto">
                     <div class="flex flex-wrap -m-2">
                         <div class="p-2 w-1/2">
                             <div class="relative">
-                                <label for="name" class="leading-7 text-sm text-gray-600">Name</label>
+                                <label for="name" class="leading-7 text-sm text-white">Name</label>
                                 <input type="text" id="name" name="name" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div class="p-2 w-1/2">
                             <div class="relative">
-                                <label for="email" class="leading-7 text-sm text-gray-600">Email</label>
+                                <label for="email" class="leading-7 text-sm text-white">Email</label>
                                 <input type="email" id="email" name="email" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"/>
                             </div>
                         </div>
                         <div class="p-2 w-full">
                             <div class="relative">
-                                <label for="message" class="leading-7 text-sm text-gray-600">Message</label>
+                                <label for="message" class="leading-7 text-sm text-white">Message</label>
                                 <textarea id="message" name="message" class="w-full bg-gray-100 bg-opacity-50 rounded border border-gray-300 focus:border-indigo-500 focus:bg-white focus:ring-2 focus:ring-indigo-200 h-32 text-base outline-none text-gray-700 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"></textarea>
                             </div>
                         </div>
                         <div class="p-2 w-full">
-                            <button class="flex mx-auto text-white bg-indigo-500 border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
+                            <button class="flex mx-auto text-black bg-white border-0 py-2 px-8 focus:outline-none hover:bg-indigo-600 rounded text-lg">Button</button>
                         </div>
                     </div>
                 </div>
@@ -38,4 +38,4 @@ const Footer = () => {
     )
 }
 
-export default Footer
+export default Contact
