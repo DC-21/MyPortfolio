271eaa60000c50b72860c0a102477b22d14887b1
DC-21 <cholahkuboko@gmail.com>
2023-06-16 14:16:26 +0200

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,10 +1,18 @@
 import React from 'react'
+import own from '../images/own.png';
 
 const About = () => {
   return (
-    <section className='flex px-4 md:px-8  flex-wrap w-full h-[600px] pt-8 '>
-      <div className='w-1/2'></div>
-      <div className='w-1/2'></div>
+    <section className='flex px-4 md:px-8  flex-wrap w-full h-[600px] pt-8 bg-slate-200'>
+      <div className='w-1/2'>
+        <div>
+            <h1>About Me</h1>
+            <p>I am a Junior Web Programmer with some experience in React JavaScript, Tailwind Css And HTML.</p>
+        </div>
+      </div>
+      <div className='w-1/2'>
+        <img src={own} alt=''/>
+      </div>
     </section>
   )
 }
