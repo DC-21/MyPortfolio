45c140481955ea5decaed7b2297c79ee268a9cb7
DC-21 <cholahkuboko@gmail.com>
2023-06-16 12:09:59 +0200

    first commit

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { TypeAnimation } from 'react-type-animation'
 import { Player } from '@lottiefiles/react-lottie-player';
 
 
@@ -7,7 +8,17 @@ const Hero = () => {
     <div className='flex px-4 md:px-8  flex-wrap w-full h-screen pt-8'>
       <div className='md:w-1/2 w-full'>
         <div>
-          <h1>Chola Kuboko</h1>
+        <TypeAnimation
+               sequence={[
+                'A Junior Web Developer', 2000,
+                'An AI Chatbot Developer', 2000,
+                'A Web Penetration Tester', 2000
+                   ]}
+                className="text-2xl w-full font-bold text-left" 
+                repeat={Infinity}
+                wrapper="p"
+                style={{ color: 'linear-gradient(to right, #fafafa, #2d177d)' }}
+               />
           <h1>Web Developer for Hire</h1>
         </div>
         <div></div>
