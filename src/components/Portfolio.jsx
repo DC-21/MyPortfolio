f9b08bc16d471db3a4d78cd18da43b1ce35cc0fc
DC-21 <cholahkuboko@gmail.com>
2023-06-16 18:14:51 +0200

    first commit

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -3,9 +3,9 @@ import Ai from '../images/aiweb.png';
 
 const Portfolio = () => {
   return (
-    <section className="text-gray-600 body-font overflow-hidden px-4 py-4 bg-blue-300">
+    <section className="text-gray-600 body-font overflow-hidden px-4 py-4 bg-white h-auto">
       <h2 className="text-center mt-4 font-bold underline text-3xl title-font text-black tracking-widest">Projects</h2>
-      <div className="container px-5 items-center justify-center mx-auto py-10">
+      <div className="container px-5 items-center justify-center mx-auto py-4">
         <div className="flex flex-wrap w-full">
           <div className="lg:w-1/2 w-full lg:pl-10 lg:py-6 lg:mt-0 flex items-center justify-center"> {/* Added flex and justify-center classes */}
             <div className="text-center">
