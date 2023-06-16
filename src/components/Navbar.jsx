5d6fb978256058a0a6f9ebf0837000584437e939
DC-21 <cholahkuboko@gmail.com>
2023-06-16 21:15:18 +0200

    first commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -26,7 +26,7 @@ const Navbar = () => {
       </ul>
       <button className="button hover:fill-white md:flex hidden hover:text-white">
         <svg className="svgIcon" viewBox="0 0 512 512" height="1em" xmlns="http://www.w3.org/2000/svg"><path d="M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zm50.7-186.9L162.4 380.6c-19.4 7.5-38.5-11.6-31-31l55.5-144.3c3.3-8.5 9.9-15.1 18.4-18.4l144.3-55.5c19.4-7.5 38.5 11.6 31 31L325.1 306.7c-3.2 8.5-9.9 15.1-18.4 18.4zM288 256a32 32 0 1 0 -64 0 32 32 0 1 0 64 0z"></path></svg>
-        Hire Me
+        <a href='https://www.fiverr.com/users/dcwrld/'>Hire Me</a>
       </button>
       <button type='button' className='md:hidden block' onClick={handleMenu}>
         {open ? <svg
