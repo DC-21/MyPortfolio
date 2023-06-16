43226b76d5416ffbc3d18223ea2b416f1ca406d5
DC-21 <cholahkuboko@gmail.com>
2023-06-16 14:22:07 +0200

    first commit

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
     <section className='flex px-4 md:px-8  flex-wrap w-full h-[600px] pt-8 bg-slate-200'>
       <div className='w-1/2'>
         <div className='text-left mt-20'>
-            <h1 className=''>About Me</h1>
+            <h1 className='font-bold text-2xl underline'>About Me</h1>
             <p>I am a Junior Web Programmer with some experience in React JavaScript, Tailwind Css And HTML.</p>
         </div>
       </div>
